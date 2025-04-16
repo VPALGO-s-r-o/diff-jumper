287e5f7162f6f0077cac36c524c9df714137f2b7
Jakub Plachy <kuba.plachy123@gmail.com>
2025-04-16 23:52:29 +0200

    add command names to const

---
+++ src/utils/commands.ts
@@ -0,0 +1,6 @@
+export const COMMANDS = {
+    // VSCode built-in commands
+    FOCUS_PRIMARY_SIDE: 'workbench.action.compareEditor.focusPrimarySide',
+    FOCUS_SECONDARY_SIDE: 'workbench.action.compareEditor.focusSecondarySide',
+    GIT_OPEN_CHANGE: 'git.openChange'
+};
